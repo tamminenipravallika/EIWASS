442dbac6415792a088854c21cd693e70797bc924
tamminenipravallika <pravallikaat@gmail.com>
2025-12-24 11:18:02 +0530

    Step 4: Connected Django project to MySQL database

--- eiwass/settings.py
+++ eiwass/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'eiwass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eiwass_db',
+        'USER': 'root',
+        'PASSWORD': 'Sssp@1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
